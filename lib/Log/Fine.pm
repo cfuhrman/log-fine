9034d897d19dc765b899a2b692c959cc6c02558d
Christopher M. Fuhrman <cfuhrman@panix.com>
2008-07-05 17:54:27 +0000

    Welcome to 0.20

--- lib/Log/Fine.pm
+++ lib/Log/Fine.pm
@@ -77,7 +77,7 @@ use Log::Fine::Logger;
 use Storable qw( dclone );
 use Sys::Syslog qw( :macros );
 
-our $VERSION = '0.15';
+our $VERSION = '0.20';
 our @ISA     = qw( Exporter );
 
 =head2 Log Levels
